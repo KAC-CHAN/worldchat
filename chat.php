aa3f3a761787a0842f4dad272c6d6ed2397a5b6c
KAC CHAN <kacchan@outlook.in>
2024-09-12 22:49:35 +0530

    Update chat.php

--- chat.php
+++ chat.php
@@ -59,7 +59,7 @@ deleteOldMessages();
 
 <!-- JavaScript code -->
 <script>
-  const socket = new WebSocket('wss://yourdomain.com:8080');
+  const socket = new WebSocket('wss://world.smmcapacity.com:8080');
 
   socket.onmessage = (event) => {
     const messageList = document.getElementById('message-list');
